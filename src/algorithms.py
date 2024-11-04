4076c9b41594bd543b97e20214603f69f6de3a52
David Vega <vegadavid46@gmail.com>
2024-11-03 23:45:44 -0500

    Insertion sort implemented

--- src/algorithms.py
+++ src/algorithms.py
@@ -32,13 +32,42 @@ def selection_sort(rectangles): # This is a generator (normal funciton with capa
 
 def insertion_sort(rectangles):
     num_rectangles = len(rectangles)
+    for i in range(1, num_rectangles):
+        min_index = i
+        min_height = rectangles[i].height
+        rectangles[i].set_smallest()
+        j=i-1
+        while j>=0 and min_height<rectangles[j].height:
+            rectangles[j].select()
+            rectangles[j+1].height = rectangles[j].height
+            j-=1
+            draw_rects(rectangles)
+            rectangles[j].unselect()
+
+        rectangles[j+1].height = min_height
+        rectangles[j+1].set_smallest()
+        min_index = j+1
+        draw_rects(rectangles)
+    
+    yield
+
+    rectangles[i].x, rectangles[min_index].x = rectangles[min_index].x, rectangles[i].x
+    rectangles[i], rectangles[min_index] = rectangles[min_index], rectangles[i]
+    
+    rectangles[min_index].unselect()
+    rectangles[i].set_sorted()
+
+    draw_rects(rectangles)
+        
+
+
     print("insertion sort algo implementation")
 def bubble_sort(rectangles):
     num_rectangles = len(rectangles)
-    print("insertion sort algo implementation")
+    print("bubble sort algo implementation")
 def quick_sort(rectangles):
     num_rectangles = len(rectangles)
-    print("insertion sort algo implementation")
+    print("quick sort algo implementation")
 
 """
 def sort_selector(option):
